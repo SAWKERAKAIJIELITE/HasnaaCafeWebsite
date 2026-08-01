b606611070569c5c2bcead42fd5c8e7c022ebf5b
SAWKERAKAIJIELITE <yesmajbesh@gmail.com>
2026-08-01 16:22:26 +0300

    trying to integrate keystatic online

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -2,7 +2,11 @@ import { config, singleton, fields, collection } from "@keystatic/core";
 
 
 export default config({
-    storage: { kind: "local", },
+    storage: {
+        // kind: "local"
+        kind: 'github',
+        repo: 'SAWKERAKAIJIELITE/HasnaaCafeWebsite'
+    },
 
     collections: {
         // heroes: collection({
